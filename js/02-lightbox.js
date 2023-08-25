65640a6ff586a8e2cce734b8d0ff1e3b20a7598c
ViktorHorbatiuk <horbatiuk.va@gmail.com>
2023-08-25 16:10:31 +0200

    Update task-02.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const containerOfGallery = document.querySelector(".gallery");
+const markupOfGallery = galleryItems
+  .map((item) => {
+    return `<li class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+            <img
+            class="gallery__image"
+            src="${item.preview}"
+            alt="${item.description}"
+            />
+        </a>
+        </li>`;
+  })
+  .join("");
+
+containerOfGallery.insertAdjacentHTML("beforeend", markupOfGallery);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
