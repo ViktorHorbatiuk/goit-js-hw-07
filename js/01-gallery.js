e2dbed4c170808f1041a984097938aeae1623f84
ViktorHorbatiuk <horbatiuk.va@gmail.com>
2023-08-25 15:10:23 +0200

    Update task-01.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const containerOfGallery = document.querySelector(".gallery");
+const markupOfGallery = galleryItems
+  .map((item) => {
+    return `<li class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+            <img
+            class="gallery__image"
+            src="${item.preview}"
+            data-source="${item.original}"
+            alt="${item.description}"
+            />
+        </a>
+        </li>`;
+  })
+  .join("");
+
+containerOfGallery.insertAdjacentHTML("beforeend", markupOfGallery);
+
+containerOfGallery.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const imageSource = e.target.dataset.source;
+  basicLightbox
+    .create(`<img src="${imageSource}" width="800" height="600">`)
+    .show();
+});
